b6d4b5817705133b7f8042ebe8e231d7caf2427b
Dimitri Samosiuk <imsweet75@gmail.com>
2021-12-20 20:33:53 +0300

    skeleton of page object first creation

---
+++ DimitriSamosiuk/src/main/java/constants/Constant.java
@@ -0,0 +1,15 @@
+package constants;
+
+public class Constant {
+
+    public static class TimeoutVariables {
+        public static final int IMPLICIT_WAIT = 5;
+        public static final int EXPLICIT_WAIT = 10;
+    }
+
+    public static class Urls {
+        public static final String GOOGLE_MAIN_PAGE_URL = "https://google.com/";
+		public static final String BELARUS_RAILWAY_NAIN_PAGE_URL = "https://rw.by/";
+    }
+}
+
