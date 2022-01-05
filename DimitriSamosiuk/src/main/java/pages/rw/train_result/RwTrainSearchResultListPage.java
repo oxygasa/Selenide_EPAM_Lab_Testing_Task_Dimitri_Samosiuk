36c8cae9942921d8d00a95b5e44c8c22b47ea71c
Dimitri Samasiuk <imsweet75@gmail.com>
2022-01-05 16:54:11 +0300

    Train search test paused. Selenide project is completed 90%

--- DimitriSamosiuk/src/main/java/pages/rw/train_result/RwTrainSearchResultListPage.java
+++ DimitriSamosiuk/src/main/java/pages/rw/train_result/RwTrainSearchResultListPage.java
@@ -1,22 +1,21 @@
 package pages.rw.train_result;
 
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import pages.base.BasePage;
-
+import static com.codeborne.selenide.Selenide.$$x;
 import static com.codeborne.selenide.Selenide.$x;
 
 public class RwTrainSearchResultListPage extends BasePage {
-    private final SelenideElement TRAIN_ROUTE_LIST_PAGE_TRAIN_ROUTE_TITLE = $x("//a[@class='sch-table__route']");
-    private final SelenideElement TRAIN_ROUTE_LIST_PAGE_ROUTE_TIME = $x("//div[contains(@class,'train-from-time')]");
+    private final ElementsCollection TRAIN_ROUTE_LIST_PAGE_TRAIN_ROUTE_TITLE = $$x("//div[@id='sch-route']//span[@class='train-route']");
+    private final ElementsCollection TRAIN_ROUTE_LIST_PAGE_ROUTE_TIME = $$x("//div[contains(@class,'train-from-time')]");
     private final SelenideElement TRAIN_ROUTE_PREVIEW_PAGE_TRAIN_ROUTE_TITLE = $x("//div[@class='sch-title__title h2']");
     private final SelenideElement TRAIN_ROUTE_PREVIEW_PAGE_DAYS_OF_ROUTE = $x("//div[@class='sch-title__descr']");
 
-    public SelenideElement getTrainRouteListPageTrainRouteTitle() {
+    public ElementsCollection getTrainRouteListPageTrainRouteTitle() {
         return TRAIN_ROUTE_LIST_PAGE_TRAIN_ROUTE_TITLE;
     }
-    public SelenideElement getTrainRouteListPageRouteTime() {
+    public ElementsCollection getTrainRouteListPageRouteTime() {
         return TRAIN_ROUTE_LIST_PAGE_ROUTE_TIME;
     }
     public SelenideElement getTrainRoutePreviewPageTrainRouteTitle() { return TRAIN_ROUTE_PREVIEW_PAGE_TRAIN_ROUTE_TITLE; }
--- DimitriSamosiuk/src/test/java/tests/rw/search/positive/PositiveRwTrainSearchTest.java
+++ DimitriSamosiuk/src/test/java/tests/rw/search/positive/PositiveRwTrainSearchTest.java
@@ -1,72 +1,66 @@
-//package tests.rw.search.positive;
-//
-//import commons.CommonActions;
-//import constants.Constant;
-//import io.qameta.allure.*;
-//import org.openqa.selenium.WebElement;
-//import org.testng.Assert;
-//import org.testng.annotations.Test;
-//import pages.rw.header.RwHeader;
-//import pages.rw.main.RwMainPage;
-//import pages.rw.train_result.RwTrainSearchResultListPage;
-//import tests.base.BaseTest;
-//import java.util.ArrayList;
-//import java.util.Arrays;
-//import java.util.List;
-//
-//public class PositiveRwTrainSearchTest extends BaseTest {
-//    RwMainPage rwMainPage = new RwMainPage(getDriver());
-//    RwTrainSearchResultListPage rwTrainSearchResultListPage = new RwTrainSearchResultListPage(getDriver());
-//    RwHeader rwHeader = new RwHeader(getDriver());
-//    @Test(priority = 1)
-//    @Description("Requirements: https://clck.ru/ZXihb")
-//    @Epic("RW008")
-//    @Feature("Train search module")
-//    @Story("As an passenger I want to find the train, " +
-//            "so let I request a train shedule via train search module.")
-//    @Severity(SeverityLevel.CRITICAL)
-//
-//    public void searchTheTrainRouteTest() {
-//        rwMainPage.
-//                goToUrl(Constant.Urls.BELARUS_RAILWAY_NAIN_PAGE_URL);
-//        rwMainPage.
-//                typeText(rwMainPage.getTrainSearchTextFieldFrom(), rwMainPage.getCityDepartureFrom());
-//        rwMainPage.
-//                typeText(rwMainPage.getTrainSearchTextFieldTo(), rwMainPage.getCityArriveTo());
-//        rwMainPage.
-//                typeText(rwMainPage.getTrainSearchDateField(), RwMainPage.getFiveDaysAfterTodayDate());
-//        rwMainPage.
-//                clickTheElement(rwMainPage.getTrainSearchSubmitButton());
-//
-//        /*** Display all tains in the console prompt**/
-//        List<WebElement> allTrainsToConsole =
-//                CommonActions.getDriver().findElements(rwTrainSearchResultListPage.getTrainRouteListPageTrainRouteTitle());
-//        List<WebElement> allTimeToConsole =
-//                CommonActions.getDriver().findElements(rwTrainSearchResultListPage.getTrainRouteListPageRouteTime());
-//        List<String> allTrainsCollection = new ArrayList<>();
-//        List<String> allTimeCollection = new ArrayList<>();
-//        for (WebElement eachElement : allTrainsToConsole) {
-//            allTrainsCollection.add(eachElement.getText());
-//        }
-//        for (WebElement eachElement : allTimeToConsole) {
-//            allTimeCollection.add(eachElement.getText());
-//        }
-//        String[] displayAllTrainsAndTime = new String[16];
-//        for (int i = 0; i < 16; i++) {
-//            displayAllTrainsAndTime[i] = allTrainsCollection.get(i);
-//            displayAllTrainsAndTime[i+1] = allTimeCollection.get(i);
-//            i++;
-//        }
-//        System.out.println(Arrays.toString(displayAllTrainsAndTime));
-//
-//        rwTrainSearchResultListPage.
-//                clickTheElement(rwTrainSearchResultListPage.getTrainRouteListPageTrainRouteTitle());
-//        rwTrainSearchResultListPage.
-//                assertElementIsDisplayed(rwTrainSearchResultListPage.getTrainRoutePreviewPageTrainRouteTitle());
-//        Assert.assertNotNull(rwTrainSearchResultListPage.getTrainRoutePreviewPageDaysOfRoute());
-//        rwMainPage.
-//                clickTheElement(rwHeader.getRwCompanyLogo());
-//        rwMainPage.
-//                assertElementIsDisplayed(rwMainPage.getTrainSearshTextFieldFromAfterClickOnLogo());
-//    }
-//}
+package tests.rw.search.positive;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.*;
+import org.testng.annotations.Test;
+import pages.rw.header.RwHeader;
+import pages.rw.main.RwMainPage;
+import pages.rw.train_result.RwTrainSearchResultListPage;
+import tests.base.BaseTest;
+
+import java.util.stream.Stream;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class PositiveRwTrainSearchTest extends BaseTest {
+    RwMainPage rwMainPage = new RwMainPage();
+    RwTrainSearchResultListPage rwTrainSearchResultListPage = new RwTrainSearchResultListPage();
+    RwHeader rwHeader = new RwHeader();
+    @Test(priority = 1)
+    @Description("Requirements: https://clck.ru/ZXihb")
+    @Epic("RW008")
+    @Feature("Train search module")
+    @Story("As an passenger I want to find the train, " +
+            "so let I request a train shedule via train search module.")
+    @Severity(SeverityLevel.CRITICAL)
+
+    public void searchTheTrainRouteTest() {
+        open(rwMainPage.getRwUrl());
+        rwMainPage
+                .getTrainSearchTextFieldFrom()
+                .setValue(rwMainPage.getCityDepartureFrom());
+        rwMainPage
+                .getTrainSearchTextFieldTo()
+                .setValue(rwMainPage.getCityArriveTo());
+        rwMainPage
+                .getTrainSearchDateField()
+                .setValue(RwMainPage.getFiveDaysAfterTodayDate());
+        rwMainPage
+                .getTrainSearchSubmitButton().click();
+
+        /*** Display all trains in the console prompt**/
+        //Need "To String"
+Stream<String> allTrainsToConsole = rwTrainSearchResultListPage.getTrainRouteListPageTrainRouteTitle().stream().map(SelenideElement::getText);
+        System.out.println(allTrainsToConsole);
+        Stream<String> allTimeToConsole = rwTrainSearchResultListPage.getTrainRouteListPageRouteTime().stream().map(SelenideElement::getText);
+        System.out.println(allTimeToConsole);
+        /*** Continue the test**/
+        //Need xpath
+        rwTrainSearchResultListPage
+                .getTrainRouteListPageTrainRouteTitle()
+                .get(0)
+                .click();
+        rwTrainSearchResultListPage
+                .getTrainRoutePreviewPageTrainRouteTitle()
+                .shouldBe(Condition.visible);
+        rwTrainSearchResultListPage
+                .getTrainRoutePreviewPageDaysOfRoute()
+                .shouldBe(Condition.readonly);
+        rwHeader
+                .getRwCompanyLogo().click();
+        rwMainPage
+                .getTrainSearchTextFieldFromAfterClickOnLogo()
+                .shouldBe(Condition.visible);
+    }
+}
